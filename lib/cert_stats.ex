d56e3d181935d73868448d83517dd2d3d646cdff
Adrian Irving-Beer <wisq@wisq.net>
2023-05-13 15:46:26 -0400

    `mix new cert_stats`

---
+++ lib/cert_stats.ex
@@ -0,0 +1,18 @@
+defmodule CertStats do
+  @moduledoc """
+  Documentation for `CertStats`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CertStats.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cert_stats_test.exs
@@ -0,0 +1,8 @@
+defmodule CertStatsTest do
+  use ExUnit.Case
+  doctest CertStats
+
+  test "greets the world" do
+    assert CertStats.hello() == :world
+  end
+end
