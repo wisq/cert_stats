910d29a55275f904a6d473b4e1dae314c9004e44
Adrian Irving-Beer <wisq@wisq.net>
2023-05-27 14:17:29 -0400

    Relax timings in Watchdog tests to avoid transient failures.
    Stretching out the delays should help avoid situations where some sort
    of scheduler delay (I assume GC?) causes a tight timing test to fail.
    
    This does mean WatchdogTest is now the slowest async test by a large
    margin, so if more tests get added, it might be worth splitting it into
    multiple async test modules.

--- test/watchdog_test.exs
+++ test/watchdog_test.exs
@@ -5,66 +5,69 @@ defmodule CertStats.WatchdogTest do
   alias CSTest.MockStatsd
 
   test "watchdog provides regular reports" do
-    {:ok, watchdog} = setup_watchdog(period_ms: 20)
+    {:ok, watchdog} = setup_watchdog()
     assert MockStatsd.next_call(watchdog) == {:record_watchdog, 0, 0}
     assert MockStatsd.next_call(watchdog) == {:record_watchdog, 0, 0}
     assert MockStatsd.next_call(watchdog) == {:record_watchdog, 0, 0}
   end
 
   test "watchdog with registered client reports healthy until deadline expires" do
-    {:ok, watchdog} = setup_watchdog(period_ms: 10)
-    Watchdog.register(:my_id, 100, watchdog)
-    assert_in_delta time_to_unhealthy(watchdog), 100, 15
+    {:ok, watchdog} = setup_watchdog()
+    Watchdog.register(:my_id, 500, watchdog)
+    assert_in_delta time_to_unhealthy(watchdog), 500, 100
   end
 
   test "watchdog success messages refresh deadline" do
-    {:ok, watchdog} = setup_watchdog(period_ms: 10)
-    Watchdog.register(:my_id, 10, watchdog)
+    {:ok, watchdog} = setup_watchdog()
+    Watchdog.register(:my_id, 50, watchdog)
 
-    # Will refresh at approx [0, 30, 60], then expire at 100.
+    # Will refresh at approx [0, 100, 200], then expire at 400.
     spawn_link(fn ->
-      Watchdog.success(:my_id, 40, watchdog)
-      Process.sleep(30)
-      Watchdog.success(:my_id, 40, watchdog)
-      Process.sleep(30)
-      Watchdog.success(:my_id, 40, watchdog)
+      Watchdog.success(:my_id, 200, watchdog)
+      Process.sleep(100)
+      Watchdog.success(:my_id, 200, watchdog)
+      Process.sleep(100)
+      Watchdog.success(:my_id, 200, watchdog)
     end)
 
-    assert_in_delta time_to_unhealthy(watchdog), 100, 15
+    assert_in_delta time_to_unhealthy(watchdog), 400, 100
   end
 
-  test "multiple watchdog register messages do not refresh deadline" do
-    {:ok, watchdog} = setup_watchdog(period_ms: 10)
-    Watchdog.register(:my_id, 100, watchdog)
+  test "watchdog register messages do not refresh deadline" do
+    {:ok, watchdog} = setup_watchdog()
+    Watchdog.register(:my_id, 50, watchdog)
 
+    # Will refresh at approx [0, 100, 200] ...
     spawn_link(fn ->
-      1..10
-      |> Enum.each(fn _ ->
-        Watchdog.register(:my_id, 100, watchdog)
-        Process.sleep(30)
-      end)
+      Watchdog.register(:my_id, 200, watchdog)
+      Process.sleep(100)
+      Watchdog.register(:my_id, 200, watchdog)
+      Process.sleep(100)
+      Watchdog.register(:my_id, 200, watchdog)
     end)
 
-    assert_in_delta time_to_unhealthy(watchdog), 100, 15
+    # ... but we should expire much sooner.
+    assert_in_delta time_to_unhealthy(watchdog), 50, 100
   end
 
   test "watchdog applies a minimum deadline extension" do
-    {:ok, watchdog} = setup_watchdog(period_ms: 10, deadline_minimum: 100)
+    {:ok, watchdog} = setup_watchdog(deadline_minimum: 300)
     Watchdog.register(:my_id, 0, watchdog)
-    assert_in_delta time_to_unhealthy(watchdog), 100, 15
+    assert_in_delta time_to_unhealthy(watchdog), 300, 100
   end
 
   test "watchdog applies a fudge factor to deadlines" do
     # multiply deadline by 3x
-    {:ok, watchdog} = setup_watchdog(period_ms: 10, deadline_fudge_factor: 2.0)
-    Watchdog.register(:my_id, 33, watchdog)
-    assert_in_delta time_to_unhealthy(watchdog), 100, 15
+    {:ok, watchdog} = setup_watchdog(deadline_fudge_factor: 2.0)
+    Watchdog.register(:my_id, 100, watchdog)
+    assert_in_delta time_to_unhealthy(watchdog), 300, 100
   end
 
-  defp setup_watchdog(opts) do
+  defp setup_watchdog(opts \\ []) do
     opts =
       opts
       |> Keyword.put(:name, nil)
+      |> Keyword.put_new(:period_ms, 50)
       |> Keyword.put_new(:deadline_minimum, 0)
       |> Keyword.put_new(:deadline_fudge_factor, 0.0)
 
